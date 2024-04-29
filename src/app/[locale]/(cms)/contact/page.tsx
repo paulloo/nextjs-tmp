51f0747262d65859691044c4467eeeb36cefbf61
ddp49080 <ddp49080@ly.com>
2024-04-29 11:15:17 +0800

    fix: remove contact page

--- src/app/[locale]/(cms)/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-import { GeneralShell } from "~/islands/wrappers/general-shell";
-
-export default function Contact() {
-  return (
-    <GeneralShell>
-      This is the "contact" page.
-    </GeneralShell>
-  );
-};
