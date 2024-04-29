c92786976630e2441713ec4ce934238d43170973
ddp49080 <ddp49080@ly.com>
2024-04-29 10:59:49 +0800

    feat: 链接 vercel mysql

---
+++ src/app/[locale]/(cms)/contact/page.tsx
@@ -0,0 +1,9 @@
+import { GeneralShell } from "~/islands/wrappers/general-shell";
+
+export default function Contact() {
+  return (
+    <GeneralShell>
+      This is the "contact" page.
+    </GeneralShell>
+  );
+};
